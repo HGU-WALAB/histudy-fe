0e5bee68ffba963dcb9f7ccf93069a6c61897310
ohinhyuk <8156217@naver.com>
2023-09-01 12:14:43 +0900

    Style: 다크모드 버튼 아이콘 및 애니메이션 적용

--- histudy-front/src/components/DarkModeToggle.js
+++ histudy-front/src/components/DarkModeToggle.js
@@ -1,14 +1,46 @@
+// import { useRecoilState } from "recoil";
+// import { Switch } from "@mui/material";
+// import { darkState } from "../store/atom";
+
+// function DarkModeToggle() {
+//   const [isDark, setIsDark] = useRecoilState(darkState);
+//   const handleChange = (event) => {
+//     setIsDark(event.target.checked);
+//   };
+//   localStorage.setItem("darkMode", isDark.toString());
+//   return <Switch checked={isDark} onChange={handleChange} />;
+// }
+
+// export default DarkModeToggle;
+
 import { useRecoilState } from "recoil";
-import { Switch } from "@mui/material";
+import { IconButton, Fade } from "@mui/material";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import Brightness2Icon from "@mui/icons-material/Brightness2";
 import { darkState } from "../store/atom";
 
 function DarkModeToggle() {
   const [isDark, setIsDark] = useRecoilState(darkState);
-  const handleChange = (event) => {
-    setIsDark(event.target.checked);
+
+  const handleChange = () => {
+    setIsDark(!isDark);
+    localStorage.setItem("darkMode", (!isDark).toString());
   };
-  localStorage.setItem("darkMode", isDark.toString());
-  return <Switch checked={isDark} onChange={handleChange} />;
+
+  return (
+    <IconButton color="inherit" onClick={handleChange}>
+      <Fade in={isDark}>
+        <Brightness2Icon
+          style={{ display: isDark ? "block" : "none", color: "yellow" }}
+        />
+      </Fade>
+      <Fade in={!isDark}>
+        <WbSunnyIcon
+          style={{ display: isDark ? "none" : "block", color: "darkOrange" }}
+        />
+      </Fade>
+    </IconButton>
+  );
 }
 
 export default DarkModeToggle;
