b3732378fbc20d753eacc195227d687a32ed64ed
ohinhyuk <8156217@naver.com>
2023-09-02 17:09:31 +0900

    Fix : 매칭된 학생 목록 로딩 문제 해결

--- histudy-front/src/pages/Manager/ManageStudent.js
+++ histudy-front/src/pages/Manager/ManageStudent.js
@@ -27,6 +27,7 @@ import Title from "../../components/Manager/Table/Title";
 import { StyledTitleFlexBox } from "./style/StyledTitleFlexBox";
 import { StyledLayout } from "./style/StyledLatout";
 import { useQuery } from "react-query";
+import LoadingLayout from "../../components/Manager/Loading/LoadingLayout";
 
 const StyledFlexButtonBox = styled(Box)({
   display: "flex",
@@ -40,30 +41,17 @@ export default function ManageStudent() {
   const [searchValue, setSearchValue] = useState("");
   const setIsLoading = useSetRecoilState(isLoadingState);
 
-  const { data, isLoading } = useQuery(["users"], readAllUsers, {
-    staleTime: 5 * 60 * 1000,
+  const { isLoading } = useQuery(["users"], readAllUsers, {
     cacheTime: 5 * 60 * 1000,
     onSuccess: (data) => {
       console.log("useQuery Data", data);
       setSearchResult(data);
       setStudentData(data);
-      // setIsLoading(false);
     },
     onError: (error) => {
       console.log("error", error);
     },
   });
-
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   readAllUsers().then((data) => {
-  //     console.log("data");
-  //     console.log(data);
-  //     setSearchResult(data);
-  //     setStudentData(data);
-  //     setIsLoading(false);
-  //   });
-  // }, []);
 
   let sheetData;
   if (studentData) {
@@ -76,26 +64,9 @@ export default function ManageStudent() {
       Friends: student.friends.map((friend) => friend.name).join(", "),
     }));
   }
-  const [page, setPage] = useState(1);
 
-  const [friendInput, setFriendInput] = useState("");
   const handleChange = (event) => {
     setSearchValue(event.target.value);
-  };
-  // useEffect(() => {
-  //   if (studentData) {
-  //     setStudentData(studentData.filter((student) => student.group !== 0));
-  //     // setStudentData(filteredData);
-  //   }
-  //   console.log(studentData);
-  // }, [studentData]);
-
-  const handleClick = (event) => {
-    const ID = event.target.id;
-    console.log(ID);
-    if (ID === "다음") setPage((prev) => prev + 1);
-    else if (ID === "이전") setPage((prev) => prev - 1);
-    else if (ID === "제출") alert("제출되었습니다.");
   };
 
   const xlsx = require("xlsx");
@@ -125,56 +96,58 @@ export default function ManageStudent() {
   const theme = useTheme();
   return (
     <StyledLayout>
-      <SideBar />
+      <LoadingLayout isLoading={isLoading}>
+        <SideBar />
 
-      <Box sx={{ width: "100%" }}>
-        <>
-          <StyledTitleFlexBox>
-            <Title text={"매칭된 학생 목록"} />
-            <TextField
-              id="search"
-              type="search"
-              value={searchValue}
-              onChange={handleChange}
-              sx={{
-                width: "30rem",
-                borderRadius: "30px",
-                mb: 4,
-                "& .MuiInputBase-root": {
+        <Box sx={{ width: "100%" }}>
+          <>
+            <StyledTitleFlexBox>
+              <Title text={"매칭된 학생 목록"} />
+              <TextField
+                id="search"
+                type="search"
+                value={searchValue}
+                onChange={handleChange}
+                sx={{
+                  width: "30rem",
                   borderRadius: "30px",
-                },
-              }}
-              InputProps={{
-                style: {
-                  backgroundColor: theme.palette.background.default,
-                },
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
-              placeholder="학생 이름, 그룹 검색"
-            />
-          </StyledTitleFlexBox>
-          {searchResult && (
-            <StudentListTable
-              data={searchResult}
-              accentColumnNum={-1}
-              longWidthColumnNum={-1}
-              type="student"
-            />
-          )}
-          <StyledFlexButtonBox>
-            <LongButton
-              name="목록 받기"
-              onClick={excelDownload}
-              bgColor="primary.main"
-              fontColor="white"
-            />
-          </StyledFlexButtonBox>
-        </>
-      </Box>
+                  mb: 4,
+                  "& .MuiInputBase-root": {
+                    borderRadius: "30px",
+                  },
+                }}
+                InputProps={{
+                  style: {
+                    backgroundColor: theme.palette.background.default,
+                  },
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                placeholder="학생 이름, 그룹 검색"
+              />
+            </StyledTitleFlexBox>
+            {searchResult && (
+              <StudentListTable
+                data={searchResult}
+                accentColumnNum={-1}
+                longWidthColumnNum={-1}
+                type="student"
+              />
+            )}
+            <StyledFlexButtonBox>
+              <LongButton
+                name="목록 받기"
+                onClick={excelDownload}
+                bgColor="primary.main"
+                fontColor="white"
+              />
+            </StyledFlexButtonBox>
+          </>
+        </Box>
+      </LoadingLayout>
     </StyledLayout>
   );
 }
