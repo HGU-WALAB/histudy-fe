044e9acb4f1fb1c6cdb1ecf6e3bcf7813614fde6
ohinhyuk <8156217@naver.com>
2023-09-02 16:11:17 +0900

    Feat : useQuery 사용을 위한 QueryClientProvider , queryClient 적용

--- histudy-front/src/App.js
+++ histudy-front/src/App.js
@@ -5,23 +5,23 @@ import ThemeProvider from "./theme";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-// const darkTheme = createTheme({
-//   palette: {
-//     mode: "dark",
-//   },
-// });
-// const lightTheme = createTheme({
-//   palette: {
-//     mode: "light",
-//   },
-// });
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // window focus 만으로도 refetch를 발생시키는 조건 해제
+    },
+  },
+});
 
 function App() {
   return (
     <ThemeProvider>
       <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-        <ARouter />
+        <QueryClientProvider client={queryClient}>
+          <ARouter />
+        </QueryClientProvider>
       </GoogleOAuthProvider>
     </ThemeProvider>
   );
