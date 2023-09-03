5c4367c009c0ecc2447b704f25209a8959d12f33
ohinhyuk <8156217@naver.com>
2023-09-03 13:06:51 +0900

    Fix : footer 반응형 수정

--- histudy-front/src/components/Footer.js
+++ histudy-front/src/components/Footer.js
@@ -28,7 +28,11 @@ export default function Footer() {
               display: "flex",
               width: "100%",
               alignItems: "center",
-              justifyContent: "space-between",
+              justifyContent: {
+                md: "space-between",
+                sm: "center",
+                xs: "center",
+              },
             }}
           >
             <Box
@@ -47,7 +51,7 @@ export default function Footer() {
             </Box>
             <Box
               sx={{
-                display: "flex",
+                display: { md: "flex", sm: "none", xs: "none" },
                 flexDirection: "column",
               }}
             >
