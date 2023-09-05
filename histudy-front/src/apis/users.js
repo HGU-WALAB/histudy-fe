0cde96b691538062b1e925c25142cff6f8cb22bc
ohinhyuk <8156217@naver.com>
2023-09-06 03:06:57 +0900

    Feat : API들에 axiosInstance 적용 완료

--- histudy-front/src/apis/users.js
+++ histudy-front/src/apis/users.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import axiosInstance from "./axiosInstance";
-
 export const postReport = async (data) => {
   const response = await axios.post(
     `${process.env.REACT_APP_BACK_BASE_URL}/api/users`,
@@ -25,53 +24,16 @@ export const userSignup = async (data) => {
 };
 
 export const autoUser = async (search) => {
-  const TOKEN = localStorage.getItem("accessToken");
-
-  const response = await axios.get(
-    `${process.env.REACT_APP_BACK_BASE_URL}/api/users?search=${search}`,
-    {
-      headers: {
-        Authorization: `Bearer ${TOKEN}}`,
-      },
-      withCredentials: true,
-    }
-  );
-  return response.data;
+  const response = await axiosInstance.get(`/api/users?search=${search}`);
+  return response;
 };
 
 export const getMyTeamUsers = async () => {
-  const TOKEN = localStorage.getItem("accessToken");
-
-  const response = await axios.get(
-    `${process.env.REACT_APP_BACK_BASE_URL}/api/team/users`,
-    {
-      headers: {
-        Authorization: `Bearer ${TOKEN}}`,
-      },
-      withCredentials: true,
-    }
-  );
-  return response.data;
+  const response = await axiosInstance.get(`/api/team/users`);
+  return response;
 };
 
-// export const getProfile = async () => {
-//   const TOKEN = localStorage.getItem("accessToken");
-
-//   const response = await axios.get(
-//     `${process.env.REACT_APP_BACK_BASE_URL}/api/users/me`,
-//     {
-//       headers: {
-//         Authorization: `Bearer ${TOKEN}}`,
-//       },
-//       withCredentials: true,
-//     }
-//   );
-//   return response.data;
-// };
-
 export const getProfile = async () => {
-  // const TOKEN = localStorage.getItem("accessToken");
-
   const response = await axiosInstance.get(`/api/users/me`);
   return response;
 };
