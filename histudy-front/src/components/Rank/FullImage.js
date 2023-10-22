3afcef58ddfab472aa00a373a3be977098cc053f
ohinhyuk <8156217@naver.com>
2023-10-23 04:30:07 +0900

    Feat: 관리자가 Rank의 사진을 클릭하면 사진을 모달로 띄우도록 기능 추가(#77)

---
+++ histudy-front/src/components/Rank/FullImage.js
@@ -0,0 +1,40 @@
+import { Box, Modal, Typography } from "@mui/material";
+import { useRecoilState } from "recoil";
+import { isShowFullImageState } from "../../store/atom";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function FullImage({ fullImageUrl }) {
+  const [isShowFullImage, setIsShowFullImage] =
+    useRecoilState(isShowFullImageState);
+
+  const handleClose = () => setIsShowFullImage(false);
+  console.log("fullImageUrl", fullImageUrl);
+  return (
+    <Modal
+      open={isShowFullImage}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box component="img" src={fullImageUrl} sx={style} />
+      {/* <Typography id="modal-modal-title" variant="h6" component="h2">
+          Text in a modal
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+      </Box> */}
+    </Modal>
+  );
+}
